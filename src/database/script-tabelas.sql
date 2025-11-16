922c57460bfaff87c339b6c67f5209740cd41136
GiovannaOliveir4 <giovanna.deoliveira@sptech.school>
2025-11-16 16:34:19 -0300

    API de cadastro e login funcionando

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,42 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql server
+*/
+CREATE DATABASE ST;
+
+USE ST;
+
+CREATE TABLE usuario (
+idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+email VARCHAR(100),
+senha VARCHAR(45)
+);
+
+CREATE TABLE personagem (
+idPersonagem INT PRIMARY KEY,
+nome VARCHAR(45),
+temporada VARCHAR(45)
+);
+
+CREATE TABLE partida (
+idPartida INT AUTO_INCREMENT,
+fkUsuario INT,
+fkPersonagem INT,
+pontuacao INT,
+dataHora DATETIME DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT pkComposta PRIMARY KEY (idPartida, fkUsuario, fkPersonagem),
+	CONSTRAINT fkPartidaUsuario FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+	CONSTRAINT fkPersonagemPartida FOREIGN KEY (fkPersonagem) REFERENCES personagem(idPersonagem)
+);
+
+INSERT INTO personagem VALUES 
+	(1, 'Onze', '1'),
+	(2, 'Mike', '1'),
+	(3, 'Lucas', '1'),
+	(4, 'Will', '1'),
+	(5, 'Dustin', '1'),
+	(6, 'Max', '2');
